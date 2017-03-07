7efba58942e0557ea404044b3128442bb194fe4a
Rodrigo Rivera <rod2693rm@gmail.com>
2017-03-07 14:02:19 +0100

    [STATIS] Interstructure
    Description des variables à utiliser

--- statis_interstructure.m
+++ statis_interstructure.m
@@ -1,5 +1,10 @@
-function [C,S] = statis_interstructure (X,M,Delta,norm)
+function [C,S] = statis_interstructure (X,M,Delta,Sup,norm)
 % Fonction de calcul de de l'interstructure pour la methode STATIS
+% X = Tableaux avec les t ?tudes
+% M = Metrique (usuelment matrice identit?)
+% Delta = Matrice diagonal avec les poids
+% Sup = Matrice avec les tableaux supplementaires
+% norm = Option si on veut faire l'analyse en prendre en compre la norme
 
 
 end
