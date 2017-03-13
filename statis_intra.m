563dd3f967f8621544553b6f2023f6a4e34571d8
MounirB <MounirB@users.noreply.github.com>
2017-03-13 16:58:51 +0100

    Changement de statis_intra, statis_inter et test
    Modification de la fonction ACP dans statis_intra et dans statis_inter
    Correction du fichier test.m et adaptation du fichier pour qu'il ne fonctionne seulement sur l'exemple des notes

--- statis_intra.m
+++ statis_intra.m
@@ -1,17 +1,20 @@
-function [ B ] = statis_intra( Wcomp, varnames, p )
+function [ B, Wd, VAPU, VEPU, corrvars ] = statis_intra( X, Wn, Wcomp, indnames, varetudes, varnames, p )
 %% Fonction de calcul de l'intrastructure pour la methode STATIS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input variables
+% X = Tableaux avec les t études
 % Wcomp = Matrice avec le Compromis entre les objets W
-%
-% Output Variables
-% B = Image euclidienne compromis des individus
-% varnames = variable de type string qui a le nom des variables
+% indnames = variable de type string qui a le nom des individus
+% varetudes = variable de type string qui a les noms des variables
 % p = pourcentage de inertie minimum requis (pour choisir le nb d'axes)
 %     par default on garde les 2 premiers axes si cette variable n'est pas
 %     definie
+%
+% Output Variables
+% B = Image euclidienne compromis des individus
+%
 % Use:
-% [ B ] = statis_intra( Wcomp, varnames,p )
+% [ B ] = statis_intra( Wcomp, indnames,p )
 %
 % Autor: Rodrigo Andres Rivera Martinez
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,11 +25,11 @@ D = 1/L*eye(L);
 Wd = Wcomp*D;
 
 [XU,VAPU, VEPU] = ACP(Wd);
-n = size(VAPU,1);
-V_pour = (VAPU*100)/sum(VAPU);
-p_tot = sum(V_pour(1:2));
+n = size(VAPU, 1);
+V_pour = (VAPU*100)/sum(VAPU); %Valeur propre_pourcentage (Inertie) ?
+p_tot = sum(V_pour(1:2)); %Pourcentage total ?
 j=2;
-if nargin > 2
+if nargin > 6
     while p > p_tot
         j=j+1;
         if j>n break; end;
@@ -48,15 +51,69 @@ xlabel(sprintf('Axe 1 (Inertie: %.2f %%)',V_pour(1)));
 ylabel(sprintf('Axe 2 (Inertie: %.2f %%)',V_pour(2)));
 title('Image euclidienne compromis des individus')
 
+disp('V_pour');
+disp(V_pour);
 if nargin <2
     for i=1:L
-        varnames{i} = sprintf('Individu %d',i);
+        indnames{i} = sprintf('Individu %d',i);
     end
 end
 
 for i=1:L
-    text(B(i,1), B(i,2),varnames(i));
+    text(B(i,1), B(i,2),indnames(i));
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Corrélations des variables avec les axes du compromis
+
+[nb_inds, nb_vars, nb_etudes] = size(X);
+nb_axes = 2;
+
+corrvars = ones(nb_etudes, nb_vars, nb_axes);
+[L,C,n] = size(X);
+for i = 1:n
+    Xc(:,:,i) = centrer_reduire(X(:,:,i),mean(mean(X(:,:,i))), std(std(X(:,:,i))));
+end
+
+for axe = 1:nb_axes
+    for var= 1:nb_vars
+        for etude= 1:nb_etudes
+            corrvars(etude, var, axe) = Xc(:,var,etude)'*D*VEPU(:,axe);
+        end;
+    end;
+end;
+
+% Plot des corrélations des variables
+
+figure;
+hold on;
+for var= 1:nb_vars
+    plot(corrvars(:,var,1), corrvars(:,var,2), '-O');
+    for i = 1: 4
+        text(corrvars(i,var,1), corrvars(i,var,2), [varnames(var) num2str(i)]);
+    end;
+end;
+
+grid on;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   Trajectoires   %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Corrélations individus
+
+
+for etude= 1:nb_etudes
+    WU(etude, :, :) = (Wn(:,:,etude) *D* VEPU) * diag(1./sqrt(VAPU)); 
+end;
+figure;
+hold on;
+for ind= 1:6
+    plot(WU(:,ind,1), WU(:,ind,2), '-O');
+    for i = 1: 4
+        text(WU(i,ind,1), WU(i,ind,2), [indnames(ind) 'An' num2str(i)]);
+    end;
+end;
+
+grid on;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 end
@@ -65,7 +122,7 @@ function [XU,VAPU, VEPU] = ACP(X)
 % Calcul ACP
 %--------------------------------
 % Recherche des valeurs et vecteurs propres
-[VEPU, VAPU] = eig(X'*X);    
+[VEPU, VAPU] = eig(X);    
 VAPU         = diag(VAPU);        
 %
 % Ordonnancement des valeurs et vecteurs propres
@@ -73,6 +130,17 @@ VAPU         = diag(VAPU);
 VAPU     = VAPU(flipud(s)); 
 VEPU     = VEPU(:,flipud(s)); 
 %
-% Nouvelles Coordonn?es (Composantes principales)
-XU = X * VEPU; 
+% Nouvelles Coordonnées (Composantes principales)
+XU = (X * VEPU) * diag(1./sqrt(VAPU)); 
+end
+
+
+function [Ac] = centrer_reduire(A,mean_A,std_A)
+%--------------------------------
+% Centrage des donn?es
+%--------------------------------
+UN = ones(size(A));
+Me = UN * mean_A;
+Ecart_type = UN * diag(std_A);
+Ac  = (A - Me)./Ecart_type;
 end
