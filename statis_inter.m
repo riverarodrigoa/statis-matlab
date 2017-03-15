8c6613e0d2e20ab94ee3489c09600c9e7e85a767
Rodrigo Rivera <rod2693rm@gmail.com>
2017-03-15 15:32:50 +0100

    [STATIS_INTER] Simplification de fonction
    — Simplification de calcul des parametres
    — Définition des paramètres par default
    — Message de bienvenue avec informations des variables par default
    
    **Nouvelle utilisation**
    Maintenant on peut appeler la fonction seulement avec la matrice X et
    les autres peuvent être définis comme paramètres par default.

--- statis_inter.m
+++ statis_inter.m
@@ -1,14 +1,14 @@
-function [Co,S,SS,RV,W,Wn,VaP,VeP,p] = statis_inter (X,M,Delta,Sup,norm,D,etunames)
+function [Co,S,SS,RV,W,Wn,VaP,VeP] = statis_inter (X,M,D,Delta,norm,etunames)
 %% Fonction de calcul de de l'interstructure pour la methode STATIS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input variables
 % X = Tableaux avec les t ?tudes
+%
+% PARAMETRES OPTIONELS
 % M = Metrique (usuelment matrice identit?)
 % Delta = Matrice diagonal avec les poids
 % Sup = Matrice avec les tableaux supplementaires
 % norm = Option si on veut faire l'analyse en prendre en compre la norme
-%
-% PARAMETRES OPTIONELS
 % varnames = variable de type string qui a le nom des variables
 % D = M?trique des poids, permettant le calcul des distances entre variables,
 %     usuelment 1/n * I (I est la matrice identit?)
@@ -23,40 +23,55 @@ function [Co,S,SS,RV,W,Wn,VaP,VeP,p] = statis_inter (X,M,Delta,Sup,norm,D,etunam
 % Wn = Matrice avec les objets des t etudes normes
 % VaP = Valeurs propres du matrice SS
 % VeP = Vecteurs propres du matrice SS
-% Xc = Donn?es centr?es et reduites
 %
 % Use:
-% [Co,S,SS,RV,W,Wn,VaP,VeP,Xc] = statis_inter (X,M,Delta,Sup,norm,D,etunames)
+% [Co,S,SS,RV,W,Wn,VaP,VeP] = statis_inter (X,M,D,Delta,norm,etunames)
 %
 % Author: Rodrigo Andres Rivera Martinez
 % Corrections: Larbi Mouchou, Mounir Bendali-Braham, Nafise Gouard
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% D?finition des objets repr?sentatifs
-% Verification des matrices
-if(nargin<5)
-    error('SYNTAXE ERROR: statis_interstructure (X,M,Delta,Sup,norm)');
-end
-
-[L,C,n] = size(X);
-[L3,C3]=size(Sup);
-
-if L~=L3
-    error('ERROR: Nb d''individus doit etre egal dans les matrices X et Sup');
-end
-if C ~= C3
-    error('ERROR: Nb de variables doit etre egal dans les matrices X et Sup');
-end
-
-if nargin<7
-    for i=1:n
-        etunames{i} = sprintf('Objet %d',i);
+disp('***************************************');
+disp('******** STATIS INTERSTRUCTURE ********');
+disp('***************************************');
+%% Definition des objets repr?sentatifs
+% Verification des matrices et definition des parametres par default
+if ~exist('X','var') || isempty(X)
+    error('[STATIS] You must provide a matrix X');
+else
+    disp('****************************');
+    disp('***Parametres par default***');
+    disp('****************************');
+    disp('');
+    [L,C,n] = size(X);
+    if ~exist('M','var') || isempty(M)
+        M = eye(C);
+        fprintf('[DEFAULT] M = I [%d x %d]\n',size(M));
     end
-else 
-    if size(etunames,2) < n || size(etunames,2) > n
-        error('ERROR: <varnames> doit etre de la meme taille que le nb d''?tudes');
+    if ~exist('D','var') || isempty(D)
+        D = 1/L * eye(L);
+        fprintf('[DEFAULT] D = 1/%d *I [%d x %d]\n',L,size(D));
+    end
+    if ~exist('Delta','var') || isempty(Delta)
+        Delta = 1/n*eye(n);
+        fprintf('[DEFAULT] Delta = 1/%d *I [%d x %d]\n',n,size(Delta));
+    end
+    if ~exist('norm','var') || isempty(norm) % norm? par default
+        norm = 1;
+        disp('[DEFAULT] Norme = 1');
+    end
+    if ~exist('etunames','var') || isempty(etunames)
+        for i=1:n
+            etunames{i} = sprintf('Objet %d',i);
+        end
+        fprintf('[DEFAULT] etunames [%d x %d]\n',size(etunames));
+    else
+        if size(etunames,2) < n || size(etunames,2) > n
+        error('ERROR: <etunames> doit etre de la meme taille que le nb d''?tudes');
+        end
     end
 end
+disp('****************************');
 %-------------------------------------------------------------------------------
 % Definition des objets
 %-------------------------------------------------------------------------------
@@ -65,45 +80,40 @@ for i = 1:n
     Xc(:,:,i) = centrer(X(:,:,i),mean(mean(X(:,:,i))));
 end
 
-for i = 1:n
-     W(:,:,i) = Xc(:,:,i)*M*Xc(:,:,i)';
-     Wn(:,:,i) = W(:,:,i)/sqrt(norme(W(:,:,i)));
+% Calcul des objets
+W = zeros(L,L,n);
+if norm
+    for i = 1:n
+        W(:,:,i) = Xc(:,:,i)*M*Xc(:,:,i)';
+        Wn(:,:,i) = W(:,:,i)/sqrt(norme(W(:,:,i),D));
+    end
+else
+    for i = 1:n
+        W(:,:,i) = Xc(:,:,i)*M*Xc(:,:,i)';
+    end
 end
 %-------------------------------------------------------------------------------
 % Calcul de la matrice des produits scalaires S
 %-------------------------------------------------------------------------------
-if nargin > 5
-    if ~norm
-        for i=1:n
-            for j=1:n
-                S(i,j)=prod_hs(W(:,:,i),W(:,:,j),D);
-            end
-        end
-    else
-        for i=1:n
-            for j=1:n
-                S(i,j)=(prod_hs(W(:,:,i),W(:,:,j),D))/(norme(W(:,:,i),D)*(norme(W(:,:,j),D)));
-            end
+S = zeros(n);
+if ~norm
+    for i=1:n
+        for j=1:n
+            S(i,j)=prod_hs(W(:,:,i),W(:,:,j),D);
         end
     end
-else 
-    if ~norm
-        for i=1:n
-            for j=1:n
-                S(i,j)=prod_hs(W(:,:,i),W(:,:,j));
-            end
-        end
-    else
-        for i=1:n
-            for j=1:n
-                S(i,j)=(prod_hs(W(:,:,i),W(:,:,j)))/(norme(W(:,:,i))*(norme(W(:,:,j))));
-            end
+else
+    for i=1:n
+        for j=1:n
+            S(i,j)=(prod_hs(W(:,:,i),W(:,:,j),D))/(norme(W(:,:,i),D)*(norme(W(:,:,j),D)));
         end
     end
 end
+
 %-------------------------------------------------------------------------------
 % Calcul de coefficient RV
 %-------------------------------------------------------------------------------
+RV = zeros(n);
 for i=1:n
     for j=1:n
         RV(i,j) = S(i,j)/(sqrt(S(i,i))*(sqrt(S(j,j))));
@@ -120,14 +130,13 @@ SS = S*Delta;
 Co = Cp(:,1:2); 
 
 % Pourcentage d'inertie
-p= (VaP*100)/sum(VaP);
+p = (VaP*100)/sum(VaP);
 
 figure;
 scatter(Co(:,1),Co(:,2)); grid on; 
 xlabel(sprintf('Axe 1 (Inertie: %.2f %%)',p(1)));
 ylabel(sprintf('Axe 2 (Inertie: %.2f %%)',p(2)));
-title('Image euclidienne des objets')
-
+title('Image euclidienne des objets','FontSize',16);
 
 for i=1:n
     text(Co(i,1), Co(i,2),etunames(i));
@@ -139,10 +148,6 @@ function [r] = prod_hs(A,B,D)
 %--------------------------------
 % Definition de produit scalaire
 %--------------------------------
-if nargin < 3
-    n= size(A,2);
-    D = 1/n * eye(n);
-end
 r = trace(D*A*D*B);
 end
 
@@ -150,11 +155,6 @@ function [An]= norme(A,D)
 %--------------------------------
 % Definition de norme
 %--------------------------------
-if nargin < 3
-    n= size(A,2);
-    D =1/n * eye(n);
-end
-
 An= sqrt(prod_hs(A,A,D));
 end
 
